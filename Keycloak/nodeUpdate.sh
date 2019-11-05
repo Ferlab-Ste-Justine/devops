c812a54fbf4ff69be60a92462dde5d22436e3a00
Eric Vallee <eric_vallee@webificservices.com>
2019-11-05 18:14:55 -0500

    Fixed keycloak node update script

--- Keycloak/nodeUpdate.sh
+++ Keycloak/nodeUpdate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker node update --label-add db_role=postgresql workflow
-docker node update --label-add node_role=devops workflow
+docker node update --label-add db_role=postgresql $(docker node ls -q)
+docker node update --label-add node_role=devops $(docker node ls -q)
