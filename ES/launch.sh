8720b123e06868e53a85555111f15fe9c1e46b6b
Patrice Laplante <plaplante@ferlab.bio>
2020-07-31 14:51:49 -0400

    Script to launch es7 on collab

---
+++ ES/launch.sh
@@ -0,0 +1,17 @@
+SWARM=$(docker node ls -q 2>&1 >/dev/null)
+if [[ $SWARM = Error* ]]; then
+    echo "Docker must be running in swarm mode to execute this script";
+    exit 1;
+fi
+
+PROXY_NETWORK_EXISTS=$(docker network ls | grep proxy)
+if [ -z "$PROXY_NETWORK_EXISTS" ]; then
+    docker network create -d overlay --attachable proxy;
+fi
+
+export DATA_DIRECTORY="/home/ubuntu/es7data";
+if [ ! -d "$DATA_DIRECTORY" ]; then
+    mkdir -p $DATA_DIRECTORY;
+fi
+
+docker stack deploy -c docker-compose.yml elasticsearch7;
